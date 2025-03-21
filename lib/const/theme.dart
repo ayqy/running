ea647b8be333cae049db2b110a21d515bc0bc3b5
huahua <panicmonkey@163.com>
2025-03-21 22:23:36 +0800

    fix: dashboard background color

--- lib/const/theme.dart
+++ lib/const/theme.dart
@@ -70,7 +70,7 @@ class ThemeColors extends ChangeNotifier {
 
   // 各主题下的背景色
   static final Map<ThemeType, Color> _backgroundColors = {
-    ThemeType.light: const Color(0xfff6f7f7),
+    ThemeType.light: Colors.white,
     ThemeType.dark: const Color(0xFF1A1A1A),
   };
 
